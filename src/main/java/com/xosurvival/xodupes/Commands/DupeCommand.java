c7b78da69ab08af856973d976cebe2eff8d8cab5
FroostySnoowman <jacob.j.beal@icloud.com>
2022-08-13 15:43:06 -0600

    XOSurvival-Dupes

---
+++ src/main/java/com/xosurvival/xodupes/Commands/DupeCommand.java
@@ -0,0 +1,47 @@
+package com.xosurvival.xodupes.Commands;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import com.xosurvival.xodupes.Main;
+import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
+import org.bukkit.Material;
+import org.bukkit.command.*;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+public class DupeCommand implements CommandExecutor {
+    private Main main;
+
+    public DupeCommand(Main main) {
+        this.main = main;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        String prefix_message = main.getConfig().getString("prefix-message");
+        String no_permission_message = main.getConfig().getString("no-permission-message");
+        String blank_hand_message = main.getConfig().getString("blank-hand-message");
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            if (sender.hasPermission("xo.dupe")) {
+                ItemStack item = player.getInventory().getItemInMainHand();
+                if (item == null) {
+                    sender.sendMessage(MiniMessage.miniMessage().deserialize(prefix_message + ' ' + blank_hand_message));
+                } else {
+                    if (player.getInventory().getItemInMainHand().equals(Material.AIR)) {
+                        sender.sendMessage(MiniMessage.miniMessage().deserialize(prefix_message + ' ' + blank_hand_message));
+                    } else {
+                        sender.sendMessage(MiniMessage.miniMessage().deserialize(String.valueOf(item)));
+                    }
+                }
+            } else {
+                sender.sendMessage(MiniMessage.miniMessage().deserialize(prefix_message + ' ' + no_permission_message, Placeholder.component("permission", Component.text("xo.dupe"))));
+            }
+        } else {
+            String non_player_message = main.getConfig().getString("non-player-message");
+            sender.sendMessage(MiniMessage.miniMessage().deserialize(non_player_message));
+        }
+        return true;
+    }
+}
+
---
+++ src/main/java/com/xosurvival/xodupes/Main.java
@@ -0,0 +1,25 @@
+package com.xosurvival.xodupes;
+
+import com.xosurvival.xodupes.Commands.*;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("XOSurvival-Dupes plugin has enabled.");
+
+        // Commands
+        getCommand("dupe").setExecutor(new DupeCommand(this));
+
+
+        // Config
+        getConfig().options().copyDefaults();
+        saveDefaultConfig();
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("XOSurvival-Dupes plugin has disabled.");
+    }
+}
